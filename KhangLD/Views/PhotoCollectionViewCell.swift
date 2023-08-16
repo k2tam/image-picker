919245e806f99b591fde0bcefcb8acf8affa2872
Tam Bui <buingockhaitam01@gmail.com>
2023-08-16 08:45:43 +0700

    add transparent view when select cell

--- KhangLD/Views/PhotoCollectionViewCell.swift
+++ KhangLD/Views/PhotoCollectionViewCell.swift
@@ -12,25 +12,31 @@ import Photos
 class PhotoCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var photoImageView: UIImageView!
     
-    let circleIndexLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+    private let transparentView = UIView()
 
+    
+    
     var indexLabel: Int? {
         didSet {
             if let indexSelected = indexLabel {
                 
-                //configure circleIndexLabel for the current cell and add to this cell
-                circleIndexLabel.center = self.contentView.center
+                transparentView.frame = contentView.bounds
+                transparentView.backgroundColor = UIColor(white: 1, alpha: 0.5)
+                
+                let circleIndexLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+                circleIndexLabel.center = transparentView.center
                 circleIndexLabel.textAlignment = .center
                 circleIndexLabel.backgroundColor = UIColor(red: 60/255, green: 77/255, blue: 110/255, alpha: 1.0)
                 circleIndexLabel.textColor = .white
                 circleIndexLabel.font = UIFont.boldSystemFont(ofSize: 14)
                 circleIndexLabel.layer.cornerRadius = circleIndexLabel.frame.size.width / 2
                 circleIndexLabel.clipsToBounds = true
-                circleIndexLabel.text = String(indexSelected+1)
+                circleIndexLabel.text = String(indexSelected + 1)
                 
-                self.addSubview(circleIndexLabel)
+                transparentView.addSubview(circleIndexLabel)
+                contentView.addSubview(transparentView)
             }else{
-                circleIndexLabel.removeFromSuperview()
+                transparentView.removeFromSuperview()
             }
         }
     }
@@ -41,17 +47,17 @@ class PhotoCollectionViewCell: UICollectionViewCell {
         super.awakeFromNib()
         self.layer.cornerRadius = 8
         self.layer.masksToBounds = true
-
+        
     }
     override func prepareForReuse() {
         
     }
     
     func cancelImageRequest() {
-            if imageRequestID != PHInvalidImageRequestID {
-                PHImageManager.default().cancelImageRequest(imageRequestID)
-                imageRequestID = PHInvalidImageRequestID
-            }
+        if imageRequestID != PHInvalidImageRequestID {
+            PHImageManager.default().cancelImageRequest(imageRequestID)
+            imageRequestID = PHInvalidImageRequestID
         }
-  
+    }
+    
 }
